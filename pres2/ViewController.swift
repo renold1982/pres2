c92a9512d9785eaa787bd026f98dcd154ad45abc
TUVPR-3 <tuvpr-3@nobody.invalid>
2016-12-30 17:32:39 +0330

    Initial Commit

---
+++ pres2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pres2
+//
+//  Created by TUVPR-3 on 12/30/16.
+//  Copyright © 2016 TUVPR-3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
